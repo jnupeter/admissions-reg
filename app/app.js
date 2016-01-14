13fce3cdaf28765ade3c1d0608a130611d797795
unknown <peter-wei.cai@QTAC-02254.qtac.edu.au>
2016-01-14 17:11:51 +1000

    add validator

--- app/app.js
+++ app/app.js
@@ -2,8 +2,23 @@
 
 var myapp = angular.module('myApp', ['ui.bootstrap']);
 
+myapp.directive('availableEmail', ['$log', function($log) {
+  return {
+     require: 'ngModel',
+	 link: function(scope, elm, attrs, ctrl) {
+	    $log.log('in directive');
+	    if (ctrl) {
+		   ctrl.$validators.unshift(function(viewValue) {
+		      $log.log('in directive');
+		      return false;
+		   });
+		}
+	 }
+  };
+}]);
+
 myapp.controller('anothercontroller', ['$scope', '$log', function($scope, $log){
-    $scope.customer = {};
+    $scope.customer = {"questions": [{},{}]};
     $scope.today = new Date();
     $scope.status = {opened:true};
     $scope.dateOptions = {
@@ -17,9 +32,16 @@ myapp.controller('anothercontroller', ['$scope', '$log', function($scope, $log){
     };
 
     $scope.create = function() {
-       $log.log($scope.customer);
+       $log.log(angular.toJson($scope.customer, true));
     };
     $scope.open = function($event) {
     	$scope.status.opened = true;
-    }
+    };
+	
+	$scope.checkForm = function() {
+	    return false;
+	};
+	$scope.questions = [{"id":1, "description": "question 1"}, {"id":2, "description": "question 2"}];
+	$scope.activeLegalDeclaration = {"content": "this is the content of legal declaration", "version": 5};
+	$scope.countries =["Australia", "China", "Chile"];
 }]);
