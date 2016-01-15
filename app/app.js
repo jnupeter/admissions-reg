28e26e187c057f24b0743e952485b24a69bacce7
unknown <peter-wei.cai@QTAC-02254.qtac.edu.au>
2016-01-15 16:34:48 +1000

    add http method

--- app/app.js
+++ app/app.js
@@ -2,23 +2,33 @@
 
 var myapp = angular.module('myApp', ['ui.bootstrap']);
 
-myapp.directive('availableEmail', ['$log', function($log) {
+myapp.directive('availableEmail', ['$q', '$log','$http', function($q, $log, $http) {
   return {
      require: 'ngModel',
 	 link: function(scope, elm, attrs, ctrl) {
-	    $log.log('in directive');
-	    if (ctrl) {
-		   ctrl.$validators.unshift(function(viewValue) {
-		      $log.log('in directive');
-		      return false;
-		   });
+        ctrl.$asyncValidators.abc = function(modelValue, viewValue) {
+		    if(ctrl.$isEmpty(modelValue)) {
+			  return $q.when();
+			}
+			var def = $q.defer();
+			
+			$http.get('http://localhost:8080/admissions-rest-api/rest/accounts/availibility/' + viewValue).success(function(data){
+			   $log.log(data);
+			   if(data.available == true) {
+			      def.resolve();
+			   } else {
+			      def.reject();
+			   }
+			});
+
+			return def.promise;
 		}
 	 }
   };
 }]);
 
-myapp.controller('anothercontroller', ['$scope', '$log', function($scope, $log){
-    $scope.customer = {"questions": [{},{}]};
+myapp.controller('anothercontroller', ['$scope', '$window','$log', '$http', function($scope, $window, $log, $http){
+    $scope.customer = {"questions": [{"sequence" : 1},{"sequence" : 2}]};
     $scope.today = new Date();
     $scope.status = {opened:true};
     $scope.dateOptions = {
@@ -33,6 +43,15 @@ myapp.controller('anothercontroller', ['$scope', '$log', function($scope, $log){
 
     $scope.create = function() {
        $log.log(angular.toJson($scope.customer, true));
+	   $http.post('http://localhost:8080/admissions-rest-api/rest/customers', angular.toJson($scope.customer))
+	   .then(function(data){
+	       $log.log(data);
+	       $window.alert(data.status);
+		}, 
+		function(errdata){
+		   $log.log(errdata);
+		   $window.alert(errdata.status);
+		});
     };
     $scope.open = function($event) {
     	$scope.status.opened = true;
@@ -41,7 +60,16 @@ myapp.controller('anothercontroller', ['$scope', '$log', function($scope, $log){
 	$scope.checkForm = function() {
 	    return false;
 	};
-	$scope.questions = [{"id":1, "description": "question 1"}, {"id":2, "description": "question 2"}];
-	$scope.activeLegalDeclaration = {"content": "this is the content of legal declaration", "version": 5};
-	$scope.countries =["Australia", "China", "Chile"];
+	
+	$http.get('http://localhost:8080/admissions-rest-api/rest/securityquestions').success(function(data){
+	   $scope.questions = data;
+	});
+	
+	$http.get('http://localhost:8080/admissions-rest-api/rest/legaldeclaration/active').success(function(data){
+	   $scope.activeLegalDeclaration = data;
+	});
+
+	$http.get('http://localhost:8080/admissions-rest-api/rest/countries').success(function(data){
+	   $scope.countries = data;
+	});
 }]);
